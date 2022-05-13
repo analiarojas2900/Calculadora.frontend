83a379887a8c2520ddb2dbf9da9d65bf2c2223ec
Sergio <steranz@gmail.com>
2022-05-13 19:35:21 -0400

    modifica codigo

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,14 +1,25 @@
+let botones = document.querySelectorAll('.btn');
+
+for (let index = 0; index < botones.length; index++) {
+
+    let element = botones[index];
+    element.addEventListener('click', () => {
+        accion(element.textContent);
+    })
+
+}
+
 function accion(valor) {
-    
-    
-    if (valor == "c"){
+
+
+    if (valor == "C") {
         document.getElementById("tResultado").value = "";
     } else if (valor == "=") {
         document.getElementById("tResultado").value = eval(document.getElementById("tResultado").value);
-    } else if (!isNaN(valor) || valor == "+" || valor == '-' || valor == '*' || valor == '/'){
+    } else if (!isNaN(valor) || valor == "+" || valor == '-' || valor == '*' || valor == '/') {
         document.getElementById("tResultado").value += valor;
     }
 
 
-    
+
 }
