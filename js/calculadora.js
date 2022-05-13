c2d554e87fd3da75a144975cbeb50d17ca1fd12a
John Doe <planetalibrechile@gmail.com>
2022-05-13 13:32:51 -0400

    pedro

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,9 +1,9 @@
 function accion(valor) {
     
     
-    if (valor == "Limpiar"){
+    if (valor == "c"){
         document.getElementById("tResultado").value = "";
-    } else if (valor == "Resultado") {
+    } else if (valor == "=") {
         document.getElementById("tResultado").value = eval(document.getElementById("tResultado").value);
     } else if (!isNaN(valor) || valor == "+" || valor == '-' || valor == '*' || valor == '/'){
         document.getElementById("tResultado").value += valor;
